40f8fc051189b0b28fedf8a93e1d7bd235349a61
Naruto-242 <Naruto242@proton.me>
2024-02-13 16:11:18 +0000

    sm6250-common: Drop Lineage Trust HAL

--- atoll.mk
+++ atoll.mk
@@ -402,10 +402,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     TimeKeep
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
